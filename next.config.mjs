a114b1e56f2a058db3b899a7a50b9ff331050c7a
Temporary User <temp@example.com>
2025-04-12 11:51:45 +0900

    next.config.mjs 수정: 정적 내보내기 설정을 주석 처리하여 기본값으로 변경하였습니다.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  output: 'export', // 정적 내보내기를 위해 주석 해제하고 활성화
+  // output: 'export', // 정적 내보내기를 위해 주석 해제하고 활성화
   images: {
     unoptimized: true,
   },
